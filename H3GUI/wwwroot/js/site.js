3cc71393dc5426e56d07494f45eb792172ca3b5f
BellyMeister <SeppiHD@hotmail.com>
2020-05-13 09:09:42 +0200

    Cleanup

--- H3GUI/wwwroot/js/site.js
+++ H3GUI/wwwroot/js/site.js
@@ -69,28 +69,9 @@ $(function () {
         });
     }
     setInterval(drawPosData, 10000);
-    PostGpsLocation();
+    setInterval(getLocation(), 10000);
 })
 
-function GetGeoLocation() {
-
-    PostGpsLocation();
-}
-//Hvis man ikke acceptere localition
-//function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-//    infoWindow.setPosition(pos);
-//    infoWindow.setContent(browserHasGeolocation ?
-//        'Error: The Geolocation service failed.' :
-//        'Error: Your browser doesn\'t support geolocation.');
-//    infoWindow.open(map);
-//}
-
-function PostGpsLocation() {
-    getLocation()
-    var userId = $("#MemberIdInput").val();
-    console.log(userId);
-    
-}
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(showPosition);
